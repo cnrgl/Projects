ad9b7abbe9a3a0e289cd7e4a03260077b607eb46
cnrgl <cnrgl22@gmail.com>
2020-12-29 13:14:51 +0300

    Update and rename consonant eraser to consonant eraser.c

--- consonant eraser.c
+++ consonant eraser.c
@@ -7,7 +7,7 @@ int main(void)
 	fgets(str,100,stdin);//kullanıcı girişi buffera yazılıyor
 		while(str[i])//string sonuna kadar tarama yapılıyor
 		{
-			if((str[i]=='a')||(str[i]=='e')||(str[i]=='i')||(str[i]=='o')||(str[i]=='u')); //karakter tespiti  sesli ise yazma
+			if((str[i]=='a')||(str[i]=='e')||(str[i]=='i')||(str[i]=='o')||(str[i]=='u')); //karakter tespiti --> sesli ise yazma
 			 else
 			   printf("%c",str[i]);
 			i++;	

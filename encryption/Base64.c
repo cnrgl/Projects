92b28057f697a814e84aab8fbcbb09a19a13871d
cnrgl <cnrgl22@gmail.com>
2021-01-08 12:15:14 +0300

    Update Base64.c

--- encryption/Base64.c
+++ encryption/Base64.c
@@ -1,4 +1,4 @@
-// written by A. Caner Gül v1 - console I/O
+// written by A. Caner Gül 
 #include <stdio.h>
 
 char b64_table[64] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'};

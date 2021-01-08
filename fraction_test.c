683d4ef8e95a44916f7023d3b4de023499678593
cnrgl <cnrgl22@gmail.com>
2021-01-08 11:52:26 +0300

    Create fraction_test.c

---
+++ fraction_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+	typedef union{
+		int x;
+		double y; // it is long float
+	}data;
+	
+int main(void)
+{
+	
+	data test;
+	
+	test.y = 10.1;
+	test.x = (int)test.y; 
+	
+	printf("test int = %d\n",test.x);
+	
+	printf("test double = %lf\n",test.y);
+	
+	printf("test fraction existence %d",((test.y-test.x)>0));	// instead of test.x = (int)test.y can be usable also
+	
+	return 0;
+}

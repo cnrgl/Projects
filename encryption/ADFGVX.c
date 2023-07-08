5adec30f1c3369335b43e95e3be0b7b03a3eac2a
cnrgl <cnrgl22@gmail.com>
2023-07-09 02:20:26 +0300

    adfgvx commit 1

--- encryption/ADFGVX.c
+++ encryption/ADFGVX.c
@@ -4,9 +4,14 @@
 
 int main(int argv, char **argc)
 {
-  
-  //still in development, need more time !!!!!!! 
+	
+	int polybius[6][6] = { 'N', 'A', '1', 'C', '3', 'H';
+							'8', 'T', 'B', '2', 'O', 'M';
+							'E', '5', 'W', 'R', 'P', 'D';
+							'4', 'F', '6', 'G', '7', 'I';
+							'9', 'J', '0', 'K', 'L', 'Q';
+							'S', 'U', 'V', 'X', 'Y', 'Z'};
   
 
-return 0;
+	return 0;
 }

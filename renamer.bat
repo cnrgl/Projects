4759cae1d3e21b607316034d1ad3c28e2c2a7613
Ahmet Caner Gül <cnrgl22@gmail.com>
2024-05-04 13:17:20 +0300

    Add files via upload

---
+++ renamer.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion 
+mkdir ".\final"
+for /f "tokens=*" %%s in ('dir /b *.efe') do (
+	for /f "tokens=1,2 delims=ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnoprqstuvwyz" %%A in ("%%~ns") do (
+			setlocal
+			set /a rem = %%A %% 2
+			if !rem! == 0 (	
+				set /a div = %%A / 2 
+				echo new number is !div!
+			move /Y "%%s" ".\final\Efield!div!.efe"
+					)
+			endlocal  
+	)
+)
